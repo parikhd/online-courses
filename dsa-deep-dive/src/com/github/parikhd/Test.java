6b34c8a2744a61bb8754d3bdc1c595ace5cbd62d
Dhruv Parikh <dhruv.parikh@outlook.com>
2020-06-08 18:59:07 +0530

    change pkg structure

--- dsa-deep-dive/src/com/github/parikhd/Test.java
+++ dsa-deep-dive/src/com/github/parikhd/Test.java
@@ -2,6 +2,8 @@ package com.github.parikhd;
 
 public class Test {
     public static void main(String[] args){
-        System.out.print("Test succeed..");
+        int[] arr = new int[2];
+        arr[0] = 10;
+        System.out.print(arr[1]);
     }
 }
