abb58daf51382064105ee21bdcfaa8ff322869fd
Dhruv Parikh <dhruv.parikh@outlook.com>
2020-06-24 21:20:20 +0530

    Decorator Example

---
+++ python-core-and-advanced/Basic/decorator.py
@@ -0,0 +1,19 @@
+def sqnum(x):
+    return x ** 2
+
+
+def decorme(fun):
+    def test(x):
+        result = fun(x)
+        result += 2
+        return result
+    return test
+
+
+print(decorme(sqnum)(20))
+
+@decorme
+def cubnum(x):
+    return x ** 3
+
+print(decorme(cubnum)(10))
